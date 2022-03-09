90088fb4d8d7e39bfaabf33b20cac3117c2f28c7
Skaaz <92156546+SebastienGervilla@users.noreply.github.com>
2022-03-09 14:00:53 +0100

    Add files via upload

---
+++ pages/activity.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/styles.css">
+    <title>Secteur d'activité</title>
+</head>
+<body>
+    <section class="dashboard">
+        <section class="header-area" id="header-area">
+            <header>
+                <div class="header-content">
+                    <div class="header-brand">
+                        <img src="../assets/icons/dashboard.png" alt="">
+                        <h2>ALT Analytics</h2>
+                    </div>
+                    <nav class="header-nav">
+                        <ul>
+                            <li><a href="../index.html">Zone géograpique</a></li>
+                            <li><a href="pages/activity.html">Secteur d'activité</a></li>
+                            <li><a href="index.html">Taille d'entreprise</a></li>
+                            <li><a href="index.html">Date de création</a></li>
+                            <li><a href="index.html">Divers</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+        </section>
+        <section class="main-area">
+            <section class="topbar-area">
+                <div class="topbar-content">
+                    <div class="options">
+                        <ul>
+                            <li><button id="first-button">Stage</button></li>
+                            <li><button id="second-button">Alternance</button></li>
+                            <li><button id="third-button">Tout</button></li>
+                        </ul>
+                    </div>
+                    <div class="account">
+                        <h3>Utilisateur</h3>
+                        <button class="account-avatar"></button>
+                    </div>
+                </div>
+            </section>
+            <section class="data-area">
+                <div class="data-content" id="data-content">
+                    
+                </div>
+            </section>
+        </section>
+    </section>
+    <script src="js/main.js"></script>
+</body>
+</html>
